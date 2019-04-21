53d2ddefb7877dd0a2edbc7ed88052d4849184ac
fimmind <grayfox19@mail.ru>
2019-04-21 12:06:41 +0300

    Move initialization from declaration

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include "vector.h"
 
+using namespace std;
+
 int main()
 {
-    Vector<int>(10);
+    Vector<int> vec;
+    int num = 0;
+    for(auto i = vec.begin(); i != vec.end(); i++)
+    {
+        cout << vec << endl;
+        *i = ++num;
+    }
+    cout << vec.begin() << endl
+        << vec << endl;
+
     return 0;
 }
--- vector.h
+++ vector.h
@@ -3,46 +3,116 @@
 #include <initializer_list>
 #include <iostream>
 
+using namespace std;
+
 template<class T> 
 class Vector
 {
 public:
-    Vector(int Size = 0)
-    {
-        if(Size < 0)
-            throw std::out_of_range("Size cant be negative");
+    Vector(int Size = 0);
+    Vector(std::initializer_list<T> list);
+    ~Vector();
 
-        _size = Size;
-        offset = 0;
-        elem = new T[_size];
-    }
-    Vector(std::initializer_list<T> list):
-        Vector(list.size())
-    {
-        
-    }
-    ~Vector()
-    {
-        delete elem;
-    }
+    int size() const;
 
-    int size() const
-    {
-        return _size;
-    }
+    T& operator[](int index);
+    const T& operator[](int index) const;
 
-    T& operator[](int index)
-    {
-        if(index >= _size || index < 0)
-            throw std::out_of_range("Index out of vector size");
-        return elem[index];
-    }
-    const T& operator[](int index) const
-    {
-        return this[index];
-    }
+    T* begin();
+    const T* cbegin() const;
+    T* end();
+    const T* cend() const;
 private:
     T* elem;
     int _size;
     int offset;
 };
+
+
+template<class T>
+Vector<T>::Vector(int Size)
+{
+    if(Size < 0)
+        throw std::out_of_range("Size cant be negative");
+
+    _size = Size;
+    offset = 0;
+    elem = new T[_size];
+}
+
+template<class T> 
+Vector<T>::Vector(std::initializer_list<T> list):
+    Vector(list.size())
+{}
+
+template<class T>
+Vector<T>::~Vector()
+{
+    delete elem;
+}
+
+
+template<class T>
+int Vector<T>::size() const
+{
+    return _size;
+}
+
+
+template<class T>
+T& Vector<T>::operator[](int index)
+{
+    if(index >= _size || index < 0)
+        throw std::out_of_range("Index out of vector size");
+    return elem[index];
+}
+
+template<class T>
+const T& Vector<T>:: operator[](int index) const
+{
+    return this[index];
+}
+
+
+
+template<class T>
+T* Vector<T>::begin()
+{
+    return &elem[0];
+
+}
+template<class T>
+const T* Vector<T>::cbegin() const
+{
+    return begin();
+}
+template<class T>
+T* Vector<T>::end()
+{
+    return begin() + _size;
+
+}
+template<class T>
+const T* Vector<T>::cend() const
+{
+    return end();
+}
+
+
+
+
+template<class T> 
+ostream& operator<<(ostream& os, const Vector<T>& vec)
+{
+    cout << "[";
+    if(vec.size())
+    {
+        cout << vec[0];
+        for(int i = 1; i < vec.size(); i++)
+        {
+            cout << ", " << vec[i];
+        }
+    }
+    cout << "]";
+    return os;
+}
